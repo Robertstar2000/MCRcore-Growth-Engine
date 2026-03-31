5bc907ab30afca2d23ce36900866faf309902a8e
Robert Mills <148657803+Robertstar2000@users.noreply.github.com>
2026-03-31 10:05:54 -0400

    Backup: src/templates/__init__.py

---
+++ src/templates/__init__.py
@@ -0,0 +1,17 @@
+"""
+MCRcore Growth Engine - Email Templates & Message Blocks
+
+Reusable outreach building blocks organized by:
+  - Buyer title (CEO, Ops, CFO, IT Mgr)
+  - Industry vertical (manufacturing, logistics, insurance, SMB)
+  - Service package (10 MCRcore offerings)
+  - Differentiator (8 competitive advantages)
+"""
+
+from src.templates.message_blocks import (
+    TITLE_BLOCKS,
+    INDUSTRY_BLOCKS,
+    PACKAGE_BLOCKS,
+    DIFFERENTIATOR_BLOCKS,
+    COMPLIANCE_FOOTER,
+)
